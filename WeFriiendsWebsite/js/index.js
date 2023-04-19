8bc4a1219620fe13d1ba2e60e6511f19dfd7e084
Kateryna Shvets <32341036+Enigma2017@users.noreply.github.com>
2023-04-19 03:39:35 +0200

    Add files via upload

---
+++ WeFriiendsWebsite/js/index.js
@@ -0,0 +1,41 @@
+const btn = document.querySelector('#btn'); //выбрана кнопка
+const fullCheck = document.querySelector('#fullCheck');
+
+
+btn.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const apiKey = '66ne5repmopp67s413jkjageqzmw3eiggubgj7xa'; // api из личного кабинета нужно взять
+  const apiUrl = 'https://api.selzy.com/en/api'; //Взята часть строки из запроса
+  const listIds = '2'; // тут я не понял какая то id группа так и не понял, ее типо можно получить из личного кабинета, по заапросу. Тут надо тебе посмотреть
+
+  const name = document.querySelector('#name').value; // отсюда получаем имя из инпута
+  const email = document.querySelector('#email').value; // здесь email
+
+ //проверка на заполненность всех полей К.
+ if(name === '' || email === ''){
+  fullCheck.innerHTML = `<span>You should fill all fields!</span>`;  
+ }else{
+  const form = document.querySelector('#form');
+  form.remove();
+  fullCheck.innerHTML = `<p>Thank you for your subcribtion!</p>
+                        <span>We will send you all the information as soon as we launch</span>`
+ }
+  
+
+  const urlSend = creatingUrl(apiUrl, apiKey, name, email, listIds); // с помощью функии creatingUrl создаем url строку
+
+  postRequest(urlSend); // эта функция открывает ссылку и по идеи должна записываться в базу, но это не точно
+});
+
+function postRequest(urlSend) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', urlSend);
+  xhr.send('POST', urlSend);
+}
+
+function creatingUrl(apiUrl, apiKey, name, email, listIds) {
+  return `${apiUrl}/subscribe?format=json&api_key=${apiKey}&list_ids=${listIds}&fields[email]=${email}&fields[Name]=${name}`;
+}
+
+
